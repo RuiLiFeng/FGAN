ae1c3a17d30ba86fa8f11d46bfc95c21d3bd8227
frl <frl1996@mail.ustc.edu.cn>
2019-10-08 19:55:31 +0800

    's'

--- Utils/vae_utils.py
+++ Utils/vae_utils.py
@@ -67,7 +67,6 @@ def SL_training_function(G, D, GD, z_, y_, ema, state_dict, config):
         # If accumulating gradients, loop multiple times
         for accumulation_index in range(config['num_G_accumulations']):
             z_.sample_()
-            y_.sample_()
             D_fake = GD(z_, y_, train_G=True, split_D=config['split_D'])
             G_loss = losses.generator_loss(D_fake) / float(config['num_G_accumulations'])
             G_loss.backward()
