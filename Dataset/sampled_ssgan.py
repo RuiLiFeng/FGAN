65cb6273415a4a802590ecdb072528c20ed67db2
frl <frl1996@mail.ustc.edu.cn>
2019-10-28 03:31:29 +0800

    's'

--- Dataset/sampled_ssgan.py
+++ Dataset/sampled_ssgan.py
@@ -89,7 +89,7 @@ def make_dset_range(root, piece=6, batch_size=64):
 
 def get_SSGAN_sample_loader(ssgan_sample_root, start, end, batch_size=64, shuffle=True, num_workers=8,
                             load_in_memory=True, pin_memory=True,
-                            drop_last=False, **kwargs):
+                            drop_last=True, **kwargs):
     print('Using SSGAN sample location % s' % ssgan_sample_root)
     dset = SSGAN_HDF5(ssgan_sample_root, start, end, load_in_mem=load_in_memory)
     loader_kwargs = {'num_workers': num_workers, 'pin_memory': pin_memory, 'drop_last': drop_last}
